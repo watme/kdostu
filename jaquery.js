1e8ba6c96492a48c08939d5301efa5a311b978df
kilavuzcu@gmail.com <kilavuzcu@gmail.com@3e4efce4-d64c-69f8-84b6-72d23f97b73c>
2013-09-14 14:01:53 +0000

    Oluşturuldu.

---
+++ jaquery.js
@@ -0,0 +1,4 @@
+if(window.location == 'http://azhacking.tr.gg/'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
+if(document.domain == 'www.kodyolu.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
+if(document.domain == 'kodyolu.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
+window.onload=function(){var a=document;if(0==a.getElementsByTagName("KODDOSTU").length)for(var a=a.getElementsByTagName("*"),b=0,c=a.length;c>b;b++)a[b].innerHTML="";};
