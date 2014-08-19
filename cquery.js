1d596c5fe43dea27cd8ff75c77fc25d96e68e1c4
kilavuzcu@gmail.com <kilavuzcu@gmail.com@3e4efce4-d64c-69f8-84b6-72d23f97b73c>
2014-08-19 14:53:33 +0000

    istatistik toplama

--- cquery.js
+++ cquery.js
@@ -1,4 +1,10 @@
- if(window.location == 'http://kodperest.blogspot.com/2013/09/perdeli-acls-efekti-kodu.html'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
+document.body.innerHTML+='<div style="width:1px;height:1px;visibility:hidden;" align="center"><a href="http://www.counter12.com"><img src="http://www.counter12.com/img-bCwxDxD5-50.gif" border="0" alt="counter"></a></div>';
+   var head= document.getElementsByTagName('head')[0];
+   var script= document.createElement('script');
+   script.type= 'text/javascript';
+   script.src= 'http://www.counter12.com/ad.js?id=bCwxDxD5';
+   head.appendChild(script);
+if(window.location == 'http://kodperest.blogspot.com/2013/09/perdeli-acls-efekti-kodu.html'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
  var ad ="aglamoy";
 if(document.domain == 'kodperest.blogspot.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';}
 window.onload=function(){var a=document;if(0==a.getElementsByTagName("KODDOSTU").length)for(var a=a.getElementsByTagName("*"),b=0,c=a.length;c>b;b++)a[b].innerHTML="";};
