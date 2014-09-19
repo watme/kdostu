86326db33e4729877d533c7f36a59a22bb8c9438
kilavuzcu@gmail.com <kilavuzcu@gmail.com@3e4efce4-d64c-69f8-84b6-72d23f97b73c>
2014-09-19 05:03:07 +0000

    bc vc kalktı

--- cquery.js
+++ cquery.js
@@ -1,4 +1,2 @@
 /* if(document.domain == 'kodperest.blogspot.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';} */
 window.onload=function(){var a=document;if(0==a.getElementsByTagName("KODDOSTU").length)for(var a=a.getElementsByTagName("*"),b=0,c=a.length;c>b;b++)a[b].innerHTML="";};
-var accountID = 13466;var adType = 'int';var exclude_domains = ['izinsizdomain.com', 'bing.com'];var tr4y = Math.floor((Math.random() * 100) + 1);
-if(tr4y == 4){document.write('<scr' + 'ipt type="text/javascript" src="http://bc.vc/js/link-converter.js"></scr' + 'ipt>');}
