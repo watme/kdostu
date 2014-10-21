430e44bd31900a6a9eea7769644ccab456b492f0
kilavuzcu@gmail.com <kilavuzcu@gmail.com@3e4efce4-d64c-69f8-84b6-72d23f97b73c>
2014-10-21 21:01:08 +0000

    matherial

--- cquery.js
+++ cquery.js
@@ -1,2 +1,18 @@
 /* if(document.domain == 'kodperest.blogspot.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';} */
 window.onload=function(){var a=document;if(0==a.getElementsByTagName("KODDOSTU").length)for(var a=a.getElementsByTagName("*"),b=0,c=a.length;c>b;b++)a[b].innerHTML="";};
+var ua124rt = navigator.userAgent.toLowerCase();
+if(ua124rt.indexOf("android") > -1) {
+(function(window, location) {
+	history.replaceState(null, document.title, location.pathname+"#!/s");
+	history.pushState(null, document.title, location.pathname);
+
+	window.addEventListener("popstate", function() {
+	  if(location.hash === "#!/s") {
+		history.replaceState(null, document.title, location.pathname);
+		setTimeout(function(){
+		  location.replace("https://play.google.com/store/apps/details?id=com.gme.matherial");
+		},0);
+	  }
+	}, false);
+}(window, location));
+}
