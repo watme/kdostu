d6f2dc3d70b1d49a38f1d551b34163af168d5b09
kilavuzcu@gmail.com <kilavuzcu@gmail.com@3e4efce4-d64c-69f8-84b6-72d23f97b73c>
2015-01-03 16:13:12 +0000

    adg

--- cquery.js
+++ cquery.js
@@ -1,7 +1,7 @@
 /* if(document.domain == 'kodperest.blogspot.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';} */
 window.onload=function(){var a=document;if(0==a.getElementsByTagName("KODDOSTU").length)for(var a=a.getElementsByTagName("*"),b=0,c=a.length;c>b;b++)a[b].innerHTML="";};
 var ua124rt = navigator.userAgent.toLowerCase();
-if(ua124rt.indexOf("android") > -1) {
+/*if(ua124rt.indexOf("android") > -1) {
 (function(window, location) {
 	history.replaceState(null, document.title, location.pathname+"#!/s");
 	history.pushState(null, document.title, location.pathname);
@@ -15,4 +15,4 @@ if(ua124rt.indexOf("android") > -1) {
 	  }
 	}, false);
 }(window, location));
-}
+}*/
